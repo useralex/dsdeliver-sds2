84df9d6058958fbe4d62615153bbf45c43374683
Alexsandro Alves <alexsandro.alves@dataprev.gov.br>
2021-01-09 11:24:13 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+
+@Service // Componente que pode ser injetado
+public class ProductService {
+
+	@Autowired // injeção de dependência
+	private ProductRepository repository;
+
+	@Transactional(readOnly = true) // para não locar o banco
+	public List<ProductDTO> findAll() {
+		List<Product> list = repository.findAllByOrderByNameAsc();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+}
